f69b58855d6d8c44d740aa120d0400e694ee4e6a
Leonardo Cavaletti <impeto.blu@gmail.com>
2022-03-23 10:55:20 +0000

    Fixed canvas ref memoization

--- src/ThreeContext.jsx
+++ src/ThreeContext.jsx
@@ -1,21 +1,34 @@
-import { createContext, useContext, useEffect, useMemo, useRef } from "react";
+import { createContext, useContext, useState, useEffect, useMemo, useCallback } from "react";
 
 export const ThreeContext = createContext();
 
 export const ThreeProvider = ({ script, children, canvas, ...props }) => {
-  const three = useMemo(script, [script]);
-  const threeCanvas = useRef(null);
+  const [initialized, setInitialized] = useState(false);
+  const three = useMemo(
+    () => (!!script.prototype?.constructor ? new script() : script()),
+    [script]
+  );
+
+  const threeCanvas = useCallback(async (node) => {
+    if (!three?.init || !node) {
+      return null;
+    }
+    await three.init({ props, canvas: node });
+    setInitialized(true);
+  });
 
   useEffect(() => {
-    if (!three) return;
-    three.init({ props, canvas: canvas?.current || threeCanvas.current });
+    if (!three?.update || !initialized) {
+      return null;
+    }
+
     const update = () => {
       three.update();
       requestAnimationFrame(update);
     };
     const loop = requestAnimationFrame(update);
     return () => cancelAnimationFrame(loop);
-  }, [three]);
+  }, [threeCanvas, initialized]);
 
   const ThreeCanvas = (props) => <canvas {...props} ref={threeCanvas}></canvas>;
 
